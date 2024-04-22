d3639286b5a3c03dc0304bb18996b6205368bb92
Kim Gabiana <khemgabz@gmail.com>
2024-04-22 18:18:23 +0800

    Generate test report

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,16 @@ module.exports = {
   roots: ['<rootDir>/test'],
   testMatch: ['**/*.test.ts'],
   transform: {
-    '^.+\\.tsx?$': 'ts-jest'
-  }
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  reporters: [
+    'default',
+    [
+      'jest-junit',
+      {
+        outputDirectory: 'test-reports',
+        outputName: 'test-report.xml',
+      },
+    ],
+  ],
 };
