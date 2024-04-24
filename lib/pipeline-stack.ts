8bb99c316c06dfe4b2ca66b805d2b892e52c1352
Kim Gabiana <khemgabz@gmail.com>
2024-04-24 13:12:46 +0800

    added service pipeline

--- lib/pipeline-stack.ts
+++ lib/pipeline-stack.ts
@@ -13,31 +13,42 @@ export class PipelineStack extends cdk.Stack {
     const pipeline = new Pipeline(this, name, {
       pipelineName: name,
       crossAccountKeys: false,
+      restartExecutionOnUpdate: true,
     });
 
-    const sourceOutput = new Artifact('SourceOutput');
+    const cdkSourceOutput = new Artifact('CdkSourceOutput');
+    const serviceSourceOutput = new Artifact('ServiceSourceOutput');
 
     pipeline.addStage({
       stageName: 'Source',
       actions: [
         new GitHubSourceAction({
-          owner: 'khemgabz',
+          owner: 'kimgabz',
           repo: 'simple-cdk-pipeline',
           branch: 'master',
           actionName: 'Pipeline_Source',
           oauthToken: cdk.SecretValue.secretsManager('github-pipelin-token'),
-          output: sourceOutput,
+          output: cdkSourceOutput,
+        }),
+        new GitHubSourceAction({
+          owner: 'kimgabz',
+          repo: 'express_lambda_server',
+          branch: 'master',
+          actionName: 'Service_Source',
+          oauthToken: cdk.SecretValue.secretsManager('github-pipelin-token'),
+          output: serviceSourceOutput,
         })
       ]
     });
 
     const cdkBuildOutput = new Artifact('CdkBuildOutput');
+    const serviceBuildOutput = new Artifact('ServiceBuildOutput');
     pipeline.addStage({
       stageName: 'Build',
       actions: [
         new CodeBuildAction({
           actionName: 'CDK_Build',
-          input: sourceOutput,
+          input: cdkSourceOutput,
           outputs: [
             cdkBuildOutput
           ],
@@ -47,7 +58,20 @@ export class PipelineStack extends cdk.Stack {
             },
             buildSpec: BuildSpec.fromSourceFilename('build-specs/cdk-build-spec.yml')
           })
-        })
+        }),
+        new CodeBuildAction({
+          actionName: 'Service_Build',
+          input: serviceSourceOutput,
+          outputs: [
+            serviceBuildOutput
+          ],
+          project: new PipelineProject(this, 'ServiceBuildProject', {
+            environment: {
+              buildImage: LinuxBuildImage.STANDARD_5_0
+            },
+            buildSpec: BuildSpec.fromSourceFilename('build-specs/service-build-spec.yml')
+          })
+        }),
       ]
     });
 
